b61508bdf0caf21861ff9893598890e9af5ce9f0
Joel Dudley <joel.dudley@barchart.com>
2014-11-07 11:10:13 -0600

    Demonstrate a no-name component.  Use it as the top level class returned by the launcher

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,16 +3,18 @@ package test;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.barchart.util.guice.Component;
 import com.barchart.util.guice.GuiceLauncher;
 
+@Component("com.barchart.test.guice-app")
 public class Test {
 
 	@Inject
-	@Named("guice-app")
-	private MyComponent component;
+	@Named("#text")
+	public String text;
 
 	public void run() {
-		System.out.println("name = " + component.name);
+		System.out.println("text = " + text);
 	}
 
 	public static void main(final String[] args) throws Exception {
